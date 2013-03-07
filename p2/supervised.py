bb1c19022e2a0bcc00042c7e4418bf0b8f2ba4f1
Chris <cjh276@cornell.edu>
2013-03-06 21:23:24 -0500

    More interface-esque things and to dos

--- p2/supervised.py
+++ p2/supervised.py
@@ -5,12 +5,21 @@ class Word:
     senses = None
 
     def __init__(self):
-        pass
+        self.senses = dict()
     
-    def get_sense_prob(self, sense):
-        pass
+    def get_sense_count(self, sense):
+        if sense in self.senses:
+            return self.senses[sense].occurrences
+        else:
+            #TO-DO: SMOOTH
+            return 0
 
     def add_sense(self, sense):
+        this.senses[sense] = Sense()
+
+    def add_feature(self, sense, feature):
+        #Check if sense is in the dictionary
+        #Call Sense.add_feature
         pass
 
 
@@ -19,16 +28,47 @@ class Sense:
     featureUnigram = None
 
     def __init__(self):
-        pass
+        self.featureUnigram = dict()
+    
+    #or perhaps add_features
+    def add_feature(self, feature):
+        if feature in self.featureUnigram:
+            self.featureUnigram[feature] += 1
+        else:
+            self.featureUnigram[feature] = 1
 
-    #If feature does not occur, return a smoothed value
     def get_feature_count(self, feature):
-        pass
+        if feature in self.featureUnigram:
+            return self.featureUnigram[feature]
+        #TO-DO: Return a smoothed value
+        return 0
 
 #For testing and such
 class Supervised:
-    
+    word_sense_dictionary = None
+
     def __init__(self):
+        this.word_sense_dictionary = dict()
+
+    #Trains the model on one line's worth of info
+    #Need to account for case, stemming, etc
+    #Senses is a list of ints
+    def train_line(context, target, senses):
+        #Lookup target in the dictionary
+        if target not in self.word_sense_dictionary:
+            self.word_sense_dictionary[target] = Word()
+        for s in senses:
+            self.word_sense_dictionary[target].add_sense(s)
+        #Call add_features(context) on the entry
+        #Update word/sense counts
+        
+        pass
+
+    #Given a train file, fill in the nested dictionary
+    def train(file):
+        pass
+
+    def test(file):
         pass
 
 
