b301a644a1babfc3052720a9cbbbc0923d078c69
Joe Cardali <joe.cardali@gmail.com>
2013-03-09 17:34:20 -0500

    parsing

--- p2/dictparse.py
+++ p2/dictparse.py
@@ -5,22 +5,18 @@ import re
 soup = BeautifulSoup(open('Dictionary.xml'), 'xml')
 top_dict = {}
 
-gloss_re = re.compile('gloss="(.*)"')
-synset_re = re.compile('synset="(.*)"')
-both_re = re.compile('gloss="(.*)" synset="(.*)"')
+synset_re = re.compile('synset="([^"]*)"')
+gloss_re = re.compile('gloss="([^"]*)"')
 
 count = 0
 for tag in soup('lexelt'):
     split = tag['item'].split('.')
-    print split[0]
+    inner_dict = {}
     for child in tag.findChildren():
-        #print gloss_re.findall(str(child))
-        print both_re.findall(str(child))
+        gloss = gloss_re.findall(str(child))
+        synset = synset_re.findall(str(child))
+        inner_dict[count] = (gloss, synset)
+        count += 1
+    top_dict[split[0]] = inner_dict
+    print top_dict
     break
-    """for child in tag.findChildren():
-        inner_dict = {}
-        cid = child['id']
-        gloss = child['gloss']
-        synset = child['synset']
-        inner_dict[int(cid) % 10] = (synset, gloss)
-    top_dict[split[0]] = inner_dict"""
