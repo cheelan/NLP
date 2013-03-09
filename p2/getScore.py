50a71fea4610ac16ddcece63166d11a35b5a964d
Chris <cjh276@cornell.edu>
2013-03-09 16:39:12 -0500

    fixed indentation in getScore

--- p2/getScore.py
+++ p2/getScore.py
@@ -12,48 +12,47 @@ class Scores:
     #Returns a dictionary with keys that strings representing lists of words, and values that are counts
     #TO-DO: Punctuation (find and replace should do the trick)
     def __init__(self):
-		print 'hi'
-		
-	def getScore(self, targetSense, featureSense):
-		consecutive = 0		# running number of consecutive overlaps
-		targetList = targetSense.split(' ')
-		featureList = featureSense.split(' ')
-		targetIndex = 0
-		score = 0
-		baseScore = 2
-		
-		while targetIndex<len(targetList):
-			if targetFeature in featureList:
-				start = targetIndex
-				checkIndex = featureList.index(targetList[targetIndex]) + 1
-				targetIndex+=1
-				consecutive = 1
-				while checkIndex<len(featureList):		# second run finds all remaining matches of targetFeature in featureList
-					if checkTargetIndex>=len(targetList):
-						#add sub-score here
-						score+=baseScore**consecutive
-						checkTargetIndex=start
-						consecutive = 0
-					else:
-						if featureList[checkIndex]==targetList[checkTargetIndex]:
-							consecutive+=1
-							checkTargetIndex+=1
-							checkIndex+=1
-						else:
-							#add sub-score here
-							#does there need to be a case where consecutive==0 because it should just be 0/1??
-							#resolved?
-							
-							if consecutive==0:
-								checkIndex+=1
-							else:
-								#add sub-score here
-								score+=baseScore**consecutive
-								consecutive=0
-								checkTargetIndex=start
-		return score
-			
-		'''
+        print 'hi'
+
+    def getScore(self, targetSense, featureSense):
+        consecutive = 0		# running number of consecutive overlaps
+        targetList = targetSense.split(' ')
+        featureList = featureSense.split(' ')
+        targetIndex = 0
+        score = 0
+        baseScore = 2
+
+        while targetIndex<len(targetList):
+            if targetFeature in featureList:
+                start = targetIndex
+                checkIndex = featureList.index(targetList[targetIndex]) + 1
+                targetIndex+=1
+                consecutive = 1
+                while checkIndex<len(featureList):		# second run finds all remaining matches of targetFeature in featureList
+                    if checkTargetIndex>=len(targetList):
+                        #add sub-score here
+                        score+=baseScore**consecutive
+                        checkTargetIndex=start
+                        consecutive = 0
+                    else:
+                        if featureList[checkIndex]==targetList[checkTargetIndex]:
+                            consecutive+=1
+                            checkTargetIndex+=1
+                            checkIndex+=1
+                        else:
+                            #add sub-score here
+                            #does there need to be a case where consecutive==0 because it should just be 0/1??
+                            #resolved?
+                            if consecutive==0:
+                                checkIndex+=1
+                            else:
+                                #add sub-score here
+                                score+=baseScore**consecutive
+                                consecutive=0
+                                checkTargetIndex=start
+        return score
+
+'''
 			targetFeature = targetList[targetIndex]
 			start = targetIndex
 			targetIndex+=1
