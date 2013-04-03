d4006fc5bcab28422de66b04fbd2e65e1371c332
Chris <cjh276@cornell.edu>
2013-04-03 17:08:26 -0400

    We're now handling logs of probabilities. God help us

--- p3/hmm.py
+++ p3/hmm.py
@@ -11,6 +11,7 @@ class Node:
     node_id = 0 #Also known as score
     n = -1 #n in n-gram
     transition_counts = []
+    ngram_model = None 
 
     #If we use NLTK for n-gram stuff, you can't add new training data to an existing model
     #Therefore, we'll need to keep track of a list of sentences, and append to that throughout
@@ -21,14 +22,12 @@ class Node:
     def __init__(self, score, n, num_states):
         self.n = n
         self.score = score
-        self.transition_probabilities = [0]*num_states
+        self.transition_counts = [0]*num_states
 
     #Get the probability of transitioning from the previous score to the current score
-    def _get_transition_probability(self, prev_score):
-
+    def get_transition_probability(self, prev_score, num_sentences):
         new_index = score_to_index(prev_score)
-        
-        return float(self.transition_counts[new_index]) / float(sum) 
+        return float(self.transition_counts[new_index]) / float(new_sentences) 
 
     #Add this sentence's n-grams to the bucket
     #Update transition counts
@@ -59,17 +58,21 @@ class HMM:
 
     #For picking the first state of a paragraph. For now, start at 0, but try fancy things later
     def gen_initial_state(self):
-        return self.prev_score
+        return 0
 
     #Given a parsed sentence and its sentiment score, train it
     def train_sentence(self, sentence, score):
         index = score_to_index(score)
         self.nodes[index].train_sentence(sentence, self.prev_score)
 
+    #Returns an approximation of the log probability of a sentence appearing in the specified state
+    def get_log_prob_from_entropy(self, sentence, state):
+        split_sentence = sentence.split(" ")
+        return self.nodes[get_index_from_score(state)].entropy(split_sentence) + math.log(len(split_sentences))
+
     #Outputs a sequence of sentiments using the viterbi algorithm
     def viterbi(self, sentence_list):
         pass
-
 
 
 est = lambda fdist, bins: LidstoneProbDist(fdist, 0.2)
@@ -80,6 +83,12 @@ lm = NgramModel(2, brown.words(categories='news'), estimator=est)
 sen = "as a coincidence he gave frank some kind of a stress relieving pill the previous night which frank took in a moment of weakness and desperation as he followed mary to this drug pad concerned that she couldnt handle the strain any longer of learning about her fathers death especially when she wasnt able to put a closure on their stormy relationship by having one last chance to talk to him she hasnt even seen her father for the last three years due to an argument"
 sen2 = "askddfesf adsafe asdfhjkbeew jbdajse joiuewoeuih hgjd jgbna jkasdaf"
 sen3 = "We congratulate the entire membership on its record of good legislation"
+
+
+
+split_sentence = sen.split(" ")
+logprob = lm.entropy(split_sentence) + math.log(len(split_sentence))
+
 perplexity = math.exp(-1. * lm.entropy(sen.split(" ")))
 perplexity2 = math.exp(-1. * lm.entropy(sen2.split(" ")))
 perplexity3 = math.exp(-1. * lm.entropy(sen3.split(" ")))
